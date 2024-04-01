8b435aa5b2ca7841be72a3ecae3e2c07634686ed
Sean Jennings <seanjennings976@gmail.com>
2024-04-01 16:08:26 +0100

    Added jetbrains settings

--- windows/configure.ps1
+++ windows/configure.ps1
@@ -25,18 +25,18 @@ $jetBrainsExtensions = @(
     @{ FileExtension=".ps1"; Associate="PS1 File"; }
 )
 AssociateFileExtensions $jetBrainsExtensions "$env:programFiles(x86)\JetBrains\JetBrains Rider 2023.3.4\bin\rider64.exe"
-##AssociateFileExtensions .py "$env:programFiles\Microsoft VS Code\Code.exe"
-##AssociateFileExtensions .txt, .json "$env:programFiles\Notepad++\notepad++.exe"
+#AssociateFileExtensions .py "$env:programFiles\Microsoft VS Code\Code.exe"
+#AssociateFileExtensions .txt, .json "$env:programFiles\Notepad++\notepad++.exe"
 
-#$name = read-host `nPlease enter your name
-#$email = read-host `nPlease enter your email address
-#
-#git config --global user.name $name
-#git config --global user.email $email
-#
-#& "~\automation\windows\configs\win10_configure.ps1"
-#& "~\automation\windows\configs\VSCode\extensions.ps1"
-#
-#Copy-Item "~\automation\windows\configs\VSCode\*.json" "$env:userprofile\AppData\Roaming\Code\User"
-#Read-Host "Computer will restart then please run .\automation\windows\disable_startups.ps1"
-#Restart-Computer
+$name = read-host `nPlease enter your name
+$email = read-host `nPlease enter your email address
+
+git config --global user.name $name
+git config --global user.email $email
+
+& "~\automation\windows\configs\win10_configure.ps1"
+& "~\automation\windows\configs\VSCode\extensions.ps1"
+
+Copy-Item "~\automation\windows\configs\VSCode\*.json" "$env:userprofile\AppData\Roaming\Code\User"
+Read-Host "Computer will restart then please run .\automation\windows\disable_startups.ps1"
+Restart-Computer
