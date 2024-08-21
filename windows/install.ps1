68e21103e1355846de6ffa148b1d74633444821d
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2024-08-21 11:21:21 +0100

    Added nuget cli

--- windows/install.ps1
+++ windows/install.ps1
@@ -18,6 +18,7 @@ choco install teamviewer -yr
 choco install malwarebytes -yr
 choco install jetbrains-rider -yr
 choco install visualstudiocode -yr
+choco install nuget.commandline -yr
 choco install azure-cosmosdb-emulator
 choco install azure-data-studio -yr
 choco install azure-cli -yr
