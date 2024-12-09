9688c2c597f522fb527e4aada0fbd04057fb3550
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2024-12-09 10:42:10 +0000

    Add Powershell 7 to setup

--- windows/machine_setup.ps1
+++ windows/machine_setup.ps1
@@ -9,6 +9,8 @@ $projectsRoot = "C:\Users\seanj\repos"
 mkdir $projectsRoot -force
 [System.Environment]::SetEnvironmentVariable('ProjectsRoot',$projectsRoot, 'User')
 
+winget install -e --id Microsoft.PowerShell #Powershell 7+
+
 write-host "`nInstalling chocolatey" -fore yellow
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
