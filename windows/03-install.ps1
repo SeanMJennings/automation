dd86bf20674d97905c0aeddcdc94852951e0c68c
Sean Jennings <seanjennings976@gmail.com>
2025-11-30 21:36:45 +0000

    switch from choco to winget for Nord

--- windows/03-install.ps1
+++ windows/03-install.ps1
@@ -7,6 +7,8 @@ winget install -e --id Postman.Postman
 winget install -e --id Microsoft.PowerShell
 winget install -e --id Microsoft.VisualStudio.2022.Professional --override "--add Microsoft.VisualStudio.Component.CoreEditor --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Workload.AzureBuildTools --includeRecommended --passive"
 winget install -e --id Microsoft.VisualStudio.2022.BuildTools --force --override "--wait --passive --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows11SDK.22000" # for arduino c++ development
+winget install -e --id NordSecurity.NordPass
+winget install -e --id NordSecurity.NordVPN
 
 choco install python -yr
 winget install -e --id=astral-sh.uv
@@ -24,8 +26,6 @@ choco install nodejs -yr
 choco install yarn -yr
 choco install nunit-console-runner -yr
 choco install notepadplusplus -yr
-choco install nordpass -yr
-choco install nordvpn -yr
 choco install zoom -yr
 choco install docker-desktop -yr
 choco install dbeaver -yr
