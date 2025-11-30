f7339b5d28ab919b8ad83d079ecbbe38d70cdc94
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2025-11-30 19:42:09 +0000

    Fix path variable syntax in AddEnvironmentPaths.ps1

--- windows/powershell/AddEnvironmentPaths.ps1
+++ windows/powershell/AddEnvironmentPaths.ps1
@@ -1,13 +1,13 @@
 $paths = 
-"${env:programFiles(x86)}\Google\Chrome\Application\", 
-"${env:programFiles}\TeamViewer",
+"$env:programFiles(x86)\Google\Chrome\Application\", 
+"$env:programFiles\TeamViewer",
 "$env:programFiles\curl\bin",
 "$env:programFiles\Git\bin",
 "$env:programFiles\Microsoft VS Code",
 "$env:programFiles\Microsoft Visual Studio\2022\Proffesional\Common7\IDE",
 "$env:programFiles\Azure Data Studio",
-"${Env:APPDATA}\Local\Programs\Rider\bin\rider64.exe",
-"${Env:APPDATA}\Local\Programs\PyCharm\bin\pycharm64.exe"
+"$env:APPDATA\Local\Programs\Rider\bin",
+"$env:APPDATA\Local\Programs\PyCharm\bin"
 
 $paths | % {
 	if (-not (($env:path -split ';') -contains $_)) {
