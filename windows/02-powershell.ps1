65d29b5004c695df4153d86d16d951722061ff48
SeanJennings <seanjennings976@gmail.com>
2025-12-15 19:37:43 +0000

    ensure powershell profile is always consistently located

--- windows/02-powershell.ps1
+++ windows/02-powershell.ps1
@@ -1,19 +1,20 @@
 $projectsRoot = [System.Environment]::GetEnvironmentVariable('ProjectsRoot', 'User')
 Set-Location $projectsRoot
+$powershellCoreProfileLocation = "$env:UserProfile\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
+$windowsPowershellProfileLocation = "$env:UserProfile\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
 
 winget install -e --id Microsoft.PowerShell #Powershell 7+
 
-Copy-Item "$projectsRoot\automation\windows\powershell\*" (split-path $PROFILE) -Recurse -Force
+Copy-Item "$projectsRoot\automation\windows\powershell\*" (split-path $powershellCoreProfileLocation) -Recurse -Force
+Copy-Item "$projectsRoot\automation\windows\powershell\*" (split-path $windowsPowershellProfileLocation) -Recurse -Force
 
 $content = {(Get-Content "$projectsRoot\automation\windows\powershell\Microsoft.PowerShell_profile.ps1")}.Invoke()
 $content.Insert(0, "`$projectsRoot = `"$projectsRoot`"") 
-$content | Set-Content $PROFILE
-
-if($PROFILE -Match "WindowsPowerShell") {
-    Write-Host `nSetting up PowerShell Core `(PowerShell 7`) profile
-    $destination = "$(($PROFILE))\.." -replace "WindowsPowerShell", "PowerShell"
-    Copy-Item "$($PROFILE)\.." $destination -Recurse -Force
-}
+mkdir $env:UserProfile\Documents\PowerShell\ -force
+mkdir $env:UserProfile\Documents\WindowsPowerShell\ -force
+$content | Set-Content $env:UserProfile\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+$content | Set-Content $env:UserProfile\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "Personal" -Value "$env:UserProfile\Documents"
 
 $settingsPath = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 $settings = Get-Content $settingsPath -Raw | ConvertFrom-Json
