98738f820fbbc5490af0179534ae9cbfa2308abf
SeanJennings <seanjennings976@gmail.com>
2025-12-15 20:18:51 +0000

    alter config

--- windows/05-configure.ps1
+++ windows/05-configure.ps1
@@ -26,7 +26,7 @@ Set-BoxstarterTaskbarOptions -Unlock -Dock Bottom -Combine Always -Size Small
 $key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
 Set-ItemProperty $key DontUsePowerShellOnWinX 0
 Set-ItemProperty $key Hidden 1
-Set-ItemProperty $key ShowSuperHidden 1
+Set-ItemProperty $key ShowSuperHidden 0
 Set-ItemProperty $key ShowTaskViewButton 0
 Stop-Process -ProcessName explorer -Force
 
